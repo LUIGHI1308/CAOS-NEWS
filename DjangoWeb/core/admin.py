920d00b227037548310880ed3117a91ac7000548
LUIGHI1308 <81392707+LUIGHI1308@users.noreply.github.com>
2021-07-08 19:42:07 -0400

    Delete DjangoWeb/core directory

--- DjangoWeb/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Categoria, registro_usuario
-
-# Register your models here.
-
-admin.site.register(Categoria)
-admin.site.register(registro_usuario)
